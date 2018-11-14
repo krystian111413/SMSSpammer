d8e7721d70496d0828f0b48b0789bb4fb8865c67
krystiankowalski <krystian111413@gmail.com>
2018-11-14 16:43:54 +0100

    Add toast service and improve sms service

--- src/app/core/service/sms.service.ts
+++ src/app/core/service/sms.service.ts
@@ -8,6 +8,7 @@ import {AndroidPermissions} from '@ionic-native/android-permissions/ngx';
 export class SmsService {
 
     constructor(private sms: SMS, private androidPermission: AndroidPermissions) {
+        this.requestPermissionIfNeeded();
     }
 
     public sendMessage(number: string, text: string) {
@@ -15,6 +16,18 @@ export class SmsService {
             if (value) {
                 this.sms.send(number, text);
             } else {
+                this.androidPermission.requestPermissions([
+                    this.androidPermission.PERMISSION.READ_SMS,
+                    this.androidPermission.PERMISSION.RECEIVE_SMS,
+                    this.androidPermission.PERMISSION.SEND_SMS
+                ]);
+            }
+        });
+    }
+
+    public requestPermissionIfNeeded() {
+        this.sms.hasPermission().then(value => {
+            if (!value) {
                 this.androidPermission.requestPermissions([
                     this.androidPermission.PERMISSION.READ_SMS,
                     this.androidPermission.PERMISSION.RECEIVE_SMS,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {SmsService} from '../core/service/sms.service';
 import {Checkbox, Input} from '@ionic/angular';
+import {ToastService} from '../core/service/toast.service';
 
 @Component({
     selector: 'app-home',
@@ -12,21 +13,28 @@ export class HomePage implements OnInit {
     @ViewChild('phone') phone: Input;
     @ViewChild('text') text: Input;
     @ViewChild('repeatCount') repeatCount: Input;
+    @ViewChild('delaySendingMessage') delaySendingMessage: Input;
     @ViewChild('indexing') indexing: Checkbox;
 
-    constructor(private smsService: SmsService) {
+    constructor(private smsService: SmsService, private toastService: ToastService) {
 
     }
 
     ngOnInit(): void {
-
     }
 
     public async onSendMessage() {
+
+        if (!this.checkInput()) {
+            this.toastService.showToast('Set all data, please!', 2000);
+            return;
+        }
+
         let defaultNumber = 100000000;
         const phoneNumber = this.phone.value;
         const text = this.text.value;
         const repeat = Number(this.repeatCount.value);
+        const delay = Number(this.delaySendingMessage.value);
         const indexing = this.indexing.checked;
 
         for (let i = 0; i < repeat; i++) {
@@ -34,12 +42,20 @@ export class HomePage implements OnInit {
                 defaultNumber++;
             }
             this.smsService.sendMessage(phoneNumber, text + defaultNumber);
-            await this.delay(300);
+            await this.delay(delay);
         }
+        this.toastService.showToast('Messages sended', 2000);
     }
 
     private delay(ms: number) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
+    private checkInput() {
+        return !(this.phone.value === ''
+            || this.phone.value.length < 9
+            || this.text.value === ''
+            || this.repeatCount.value === ''
+            || this.delaySendingMessage.value === '');
+    }
 }
